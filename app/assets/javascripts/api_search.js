ba9a55bc900fd08cb8a32f67fb80f24f431c6615
Ben Willman <benjamin.willman@gsa.gov>
2016-10-23 23:21:33 -0400

    added back app/assets/javascripts/api_search.js

--- app/assets/javascripts/api_search.js
+++ app/assets/javascripts/api_search.js
@@ -2,24 +2,34 @@
 
 
 
-function get_product() {
+function fetchAPI() {
 
-	var ri_num_value = $('#ri_num').val();
+	var admin_num_value = $('#admin_num').val();
 	
-	var url = api_url + ":3000/products/" + ri_num_value;
+	//var url = "https://w2032pcctr584.aa.ad.epa.gov/dctm-rest/repositories/prismenvdev6.json";
+	var url = "https://dcoppdoctstw01.epa.gov/dctm-rest/repositories/prismtest.json";
+
+	//var dql_string_1 = "select r_object_id,object_name,title,r_creator_name,r_object_type from opp_study WHERE r_object_type='opp_study' and ANY mrid in ('" + mrid_value + "')";
+
+	var dql_string_2 = "select *,admin_num from opp_study WHERE ANY admin_num LIKE '" + admin_num_value + "%'";
+	console.log(url + "?dql=" + dql_string_2);
+
 
 	$.ajax({
-	  type: "get",
+	  type: "post",
 	  url: url,
+	  	  data: { dql: dql_string_2},
       crossDomain: true,
-      //xhrFields: {
-      	//withCredentials: true
-      //},
+      xhrFields: {
+      	withCredentials: true
+      },
       success: function (data, status, jqXHR){
     	
+    	console.log ("title:" + data.entries["0"].content.properties.object_name);
+    	docRes = data;
+    	console.log(docRes);
     	var i = 1;
-
-    	loadProduct(data);
+    	loadTiles();
     	}
 
 	});
